3c13d67aadef6364e5d3a9871b5abc586bb0d998
SamerMaz <samer.mazraani96@gmail.com>
2021-02-08 22:41:28 +0200

    1st commit

---
+++ Rocky/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rocky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Data
+{
+    //to create a Db Context for the project
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        //create a property to push them to database
+        public DbSet<Category> Category { get; set; }
+
+        public DbSet<ApplicationType> ApplicationType { get; set; }
+
+
+    }
+}
